085e756fbe11bc9689d0e83b0267a3754888b658
Dan Apanasewicz <dapanasewicz@gmail.com>
2020-07-20 17:03:40 -0400

    kill the golfers?

--- Program.cs
+++ Program.cs
@@ -9,6 +9,20 @@ namespace GitVisStudioTest
             Console.WriteLine("This Crowd has gone deadly silent.");
             Console.WriteLine("Cinderella story, out of nowhere, former greenskeeper, now about to become the Masters champion.");
             Console.WriteLine("It looks like a mirac . . .");
+            Console.WriteLine();
+            Console.WriteLine("---------------");
+            Console.WriteLine();
+            Console.WriteLine("Licensed to kill gophers by the government of the United Nations.");
+            Console.WriteLine("A man, free to kill gophers at will. To kill, you must know your enemy,");
+            Console.WriteLine("and in this case my enemy is a varmint. And a varmint will never quit -- ever.");
+            Console.WriteLine("They're like the Viet Cong -- Varmint Cong. So you have to fall back");
+            Console.WriteLine("on superior intelligence and superior firepower. And that's all she wrote.");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("press any key to exit...");
+            
+            // wait for user input to exit
+            Console.Read();
         }
     }
 }
