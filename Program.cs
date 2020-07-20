f7e0665e73df258999dd9667f15a4306795a6c72
Brian DePascale <bdepascale@gmail.com>
2020-07-20 16:01:12 -0400

    a Cinderella story

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace GitVisStudioTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This Crowd has gone deadly silent.");
+            Console.WriteLine("Cinderella story, out of nowhere, former greenskeeper, now about to become the Masters champion.");
+            Console.WriteLine("It looks like a mirac . . .");
         }
     }
 }
