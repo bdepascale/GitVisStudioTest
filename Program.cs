90f31d6ab228f6aea6ba3098bedcd0945aaba265
Brian DePascale <bdepascale@gmail.com>
2020-07-27 14:55:41 -0400

    Like the changes! Also added missing line

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace GitVisStudioTest
             Console.WriteLine("This Crowd has gone deadly silent.");
             Console.WriteLine("Cinderella story, out of nowhere, former greenskeeper, now about to become the Masters champion.");
             Console.WriteLine("It looks like a mirac . . .");
+            Console.WriteLine("It’s in the hole! IT’S IN THE HOLE!!!");
             Console.WriteLine();
             Console.WriteLine("---------------");
             Console.WriteLine();
